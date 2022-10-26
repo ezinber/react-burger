9f4c99f225ed0119b00fbb385a20a83ff76fd6e2
ezinber <ezinber@yandex.ru>
2022-10-26 21:27:39 +0300

    feat: add order button and total price

--- src/components/burger-constructor/burger-construcor.js
+++ src/components/burger-constructor/burger-construcor.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-constructor.module.css';
 
 function BurgerConstructor({ data }) {
   const [ingredients, setIngredients] = useState([]);
   const [bun, setBun] = useState({});
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     setIngredients(data.filter(i => i.type !== 'bun'));
     setBun(data.find(i => i.type === 'bun'));
+    setTotalPrice(data.reduce((total, item) => {
+      return total + item.price;
+    }, 0));
   }, [data]);
 
   return (
-    <section className={`${styles.section} pt-25 pr-4 pl-4`}>
-      <ul className={styles.list}>
-        <li className="mb-4">
+    <section className={`${styles.section} pt-25 pl-4`}>
+      <ul className={`${styles.list} mb-10`}>
+        <li className="ml-8 mb-4">
           <ConstructorElement
             type="top"
             isLocked={true}
@@ -26,18 +30,18 @@ function BurgerConstructor({ data }) {
         <li className={styles.inner_list_wrapper}>
           <ul className={styles.list}>
             {ingredients.map((item, index) => (
-              <li className="mb-4">
+              <li className="mb-4" key={`${index}${item._id}`}>
+                <DragIcon type="primary" />
                 <ConstructorElement
                   text={item.name}
                   thumbnail={item.image}
                   price={item.price}
-                  key={`${index}${item._id}`}
                 />
               </li>
             ))}
           </ul>
         </li>
-        <li className="mt-4">
+        <li className="ml-8 mt-4">
           <ConstructorElement
             type="bottom"
             isLocked={true}
@@ -47,7 +51,17 @@ function BurgerConstructor({ data }) {
           />
         </li>
       </ul>
-        
+      <div className={styles.button_wrapper}>
+        <p className="text text_type_digits-medium mr-10">
+          {totalPrice}
+          <span className="ml-2">
+            <CurrencyIcon type="primary" />
+          </span>
+        </p>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
     </section>
   )
 }
--- src/components/ingredient-card/ingredient-card.js
+++ src/components/ingredient-card/ingredient-card.js
@@ -7,7 +7,7 @@ function IngredientCard({ ingredient }) {
     <li className={`${styles.card} mb-8`}>
       <img className="mb-1" src={ingredient.image} alt={ingredient.name} />
       <div className={styles.price}>
-        <p className="text text_type_main-default">
+        <p className="text text text_type_digits-default">
           {ingredient.price}
         </p>
         <CurrencyIcon type="primary" />
